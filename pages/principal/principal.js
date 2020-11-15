a70442d12a35aa0b4c7effd3dcf69203a198f3d1
Bruno Helou <brunorhelou@gmail.com>
2020-11-15 20:59:32 -0300

    fix bugs and fav bar added

--- pages/principal/principal.js
+++ pages/principal/principal.js
@@ -1,45 +1,98 @@
-window.onload = (event) => {
 
+window.onload = (event) => {
+   
 }
 var app = new Vue({
     el: "#app",
     data: {
         glasses: {
             glass1: {
-                type: "hello",
-                model: "world",
+                type: "Aviator",
+                model: "Aviator x-10",
                 price: "R$ 3,50",
                 style: "",
                 img: "../images/1-removebg-preview.png",
             },
             glass2: {
                 type: "hello",
-                model: "world",
-                price: "",
+                model: "Quadrado surf",
+                price: "R$ 10,50",
                 style: "",
                 img: "../images/2-removebg-preview.png",
             },
             glass3: {
                 type: "hello",
-                model: "world",
-                price: "",
+                model: "Styled mix",
+                price: "R$ 4,65",
                 style: "",
                 img: "../images/3-removebg-preview.png",
             },
             glass4: {
                 type: "hello",
-                model: "world",
-                price: "",
+                model: "Retro Quadrado",
+                price: "R$ 10,58",
                 style: "",
                 img: "../images/4-removebg-preview.png",
             },
-            items: [],
-            pesquisa: "",
-        }
+            glass5: {
+                type: "hello",
+                model: "Retro Quadrado",
+                price: "R$ 10,58",
+                style: "",
+                img: "../images/4-removebg-preview.png",
+            },
+            glass6: {
+                type: "hello",
+                model: "Retro Quadrado",
+                price: "R$ 10,58",
+                style: "",
+                img: "../images/4-removebg-preview.png",
+            },
+        },
+        favs: []
+       
     },
     methods: {
         helo() {
-            this.glass1.type;
+            console.log(this.glasses);
+        },
+        fav(glass) {
+            let flag= 0;
+            if(this.favs.length > 0){
+                console.log('if');
+            for (let i in this.favs){
+                if(this.favs[i].model != glass.model){
+
+                }else{
+                   flag=1;
+            }
+           }
+           if(flag!=1){
+            this.favs.push(glass);
+            window.alert('adicionado aos favoritos');
+           }else{
+            window.alert('Já está no favoritos');
+           }
+            console.log(this.favs);
+        }else{
+            console.log('else', this.favs.length);
+            this.favs.push(glass);
         }
     }
+    },
+   
 });
+
+function w3_open() {
+  //  document.getElementById("main").style.marginLeft = "25%";
+  //document.getElementById("mySidebar").style.width = "right:0";
+    //document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+   // document.getElementById("openNav").style.display = 'none';
+  }
+  function w3_close() {
+    console.log('close');
+    
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
