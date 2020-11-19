aab083c9082f9f0f71bfec4cfec641b401ca72ae
MarileneGarcia <marilenegarcia1998@gmail.com>
2020-11-18 21:33:53 -0300

    links

--- pages/login/login.js
+++ pages/login/login.js
@@ -49,3 +49,18 @@ var app = new Vue({
 if (login == true) {
     window.open("../user/user_data.html", "_self");
 }
+
+function w3_open() {
+    //  document.getElementById("main").style.marginLeft = "25%";
+    //document.getElementById("mySidebar").style.width = "right:0";
+    //document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    // document.getElementById("openNav").style.display = 'none';
+}
+
+function w3_close() {
+    console.log('close');
+
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
