ad4813f83ef8411112dc90bb64583a618f2d16e5
MarileneGarcia <marilenegarcia1998@gmail.com>
2020-11-18 21:04:07 -0300

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,5 @@
+var login = false;
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -5,30 +7,36 @@ var app = new Vue({
         sobrenome: null,
         email: null,
         senha: null,
-        confirmar_senha:null,
+        confirmar_senha: null,
+        email_login: null,
+        senha_login: null,
     },
-    methods:{
-        checagem_login: function(e){
-            if (this.email && this.senha) {
+    methods: {
+        checagem_login: function(e) {
+            if (this.email_login && this.senha_login) {
                 // enviar para o banco de dados //
                 alert("Login feito com sucesso");
                 window.open("../user/user_data.html", "_self");
+                login = true;
             } else {
                 alert("Por favor, preencher todos os campos");
+                login = false;
             }
             e.preventDefault();
         },
 
-        checagem_cadastro: function(e){
-            if(this.nome && this.sobrenome && this.email && this.senha && this.confirmar_senha){
-                if(this.senha == this.confirmar_senha){
+        checagem_cadastro: function(e) {
+            if (this.nome && this.sobrenome && this.email && this.senha && this.confirmar_senha) {
+                if (this.senha == this.confirmar_senha) {
                     // enviar para o banco de dados //
-                alert("Conta criado com sucesso");
-                window.open("../user/user_data.html", "_self");
-                }else{
+                    login = true;
+                    alert("Conta criado com sucesso");
+                    window.open("../user/user_data.html", "_self");
+                } else {
                     alert("As duas senhas devem ser iguais");
+                    login = false;
                 }
-            }else {
+            } else {
                 alert("Por favor, preencher todos os campos");
             }
             e.preventDefault();
@@ -36,3 +44,7 @@ var app = new Vue({
     }
 
 });
+
+if (login == true) {
+    window.open("../user/user_data.html", "_self");
+}
